095c1a6f2c5ff5e762db3544adfa8c33b95bca31
PreVoxX <franciszek.kluza01@gmail.com>
2024-12-18 11:55:47 +0100

    .

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-x =12 
-print(x)
+print("sjema")
+print("https://github.com/PreVoxX/pythonek-project.git")
+
+n=int(input("podaj n"))
+suma=0
+for i in range(1, n+1):
+    suma+=1
+print(suma)
+
+def suma_rekurencyjna(n):
+    if n==0:
+        return 0
+    return n+suma_rekurencyjna(n-1)
+print(suma_rekurencyjna(n))
